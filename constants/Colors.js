337c87fce7b69f23b787a6021a403077ddb473e6
John Robles <john.robles1@upr.edu>
2020-07-23 17:07:31 -0400

    Meals App Done

---
+++ constants/Colors.js
@@ -0,0 +1,8 @@
+export default {
+    primaryColor: '#4a148c',
+    accentColor: '#ff6f00',
+    secondaryColor: '#ee3867',
+    thirdColor: 'e8edf7',
+    forthColor: 'fcb316',
+    fifthColor: '3b3052'
+};
